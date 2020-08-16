e86a19b9e644a5c23b5e5c3b76418facdf95fe54
jaigansa <jaigansa@gmail.com>
2020-08-16 10:42:04 +0530

    hello-reloc

---
+++ quick_chat/script/main.js
@@ -0,0 +1,37 @@
+function num(val) {
+    document.getElementById("telphone").value += val;
+}
+
+
+
+function chat() {
+    no = document.getElementById("telphone").value;
+    txt = document.getElementById("txt").value;
+    if (no.length === 10) {
+        var pre = "https://wa.me/91";
+        var txt = encodeURI(txt);
+        var final = pre + no + "?text=" + txt;
+        console.log(final);
+        window.open(final, "_blank");
+    } else {
+        alert("enter ten number");
+    }
+}
+
+
+function clears() {
+    x = document.getElementById("telphone").value;
+    y = x.slice(0, -1);
+    document.getElementById("telphone").value = y;
+
+}
+
+function allclear() {
+    document.getElementById("telphone").value = "";
+    document.getElementById("txt").value = "";
+}
+
+function textAreaAdjust(element) {
+    element.style.height = "1px";
+    element.style.height = (25 + element.scrollHeight) + "px";
+}
