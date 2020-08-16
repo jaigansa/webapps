adeaf420b05f5398d185a49d7d0865ed36537820
ஜெய் கணேஷ் <60472267+jaigansa@users.noreply.github.com>
2020-08-16 20:20:37 +0530

    Update main.js

--- quick_chat/script/main.js
+++ quick_chat/script/main.js
@@ -1,36 +1,67 @@
+/* WhatsApp quick Chat using url open method */
+
+/*
+  ___          _        _      ____  _             _   
+ / _ \  _   _ (_)  ___ | | __ / ___|| |__    __ _ | |_ 
+| | | || | | || | / __|| |/ /| |    | '_ \  / _` || __|
+| |_| || |_| || || (__ |   < | |___ | | | || (_| || |_ 
+ \__\_\ \__,_||_| \___||_|\_\ \____||_| |_| \__,_| \__|
+*/
+
+// Number Pad
 function num(val) {
-    document.getElementById("telphone").value += val;
+    document.getElementById("cellnum").value += val;
+}
+
+// number only
+function cellnumonly(input) {
+    var reqex = /[^0-9]/gi;
+    input.value = input.value.replace(reqex, "");
 }
 
 
+// chat button
 
-function chat() {
-    no = document.getElementById("telphone").value;
-    txt = document.getElementById("txt").value;
-    if (no.length === 10) {
-        var pre = "https://wa.me/91";
-        var txt = encodeURI(txt);
-        var final = pre + no + "?text=" + txt;
-        console.log(final);
+//variable 
+const pre = "https://wa.me/";
+const addtext = "?text=";
+// get datas
+
+
+
+
+
+
+
+function chat_open() {
+    var dcod = document.getElementById("dCode").value;
+    var cn = document.getElementById("cellnum").value;
+    var txt = document.getElementById("txt").value; // get message data
+
+    if (cn.length === 10) {
+        newtxt = encodeURI(txt);
+        final = pre + dcod + cn + addtext + newtxt;
+        //alert(final);
         window.open(final, "_blank");
     } else {
         alert("enter ten number");
     }
 }
 
-
+//clear buttom
 function clears() {
-    x = document.getElementById("telphone").value;
+    x = document.getElementById("cellnum").value;
     y = x.slice(0, -1);
-    document.getElementById("telphone").value = y;
+    document.getElementById("cellnum").value = y;
 
 }
-
+// allclear button
 function allclear() {
-    document.getElementById("telphone").value = "";
+    document.getElementById("cellnum").value = "";
     document.getElementById("txt").value = "";
 }
 
+//textarea auto height
 function textAreaAdjust(element) {
     element.style.height = "1px";
     element.style.height = (1 + element.scrollHeight) + "px";
