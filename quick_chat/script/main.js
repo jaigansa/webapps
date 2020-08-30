0d51372d0c1f30dcdbcad87e96ae8bc3d022152c
ஜெய் கணேஷ் <60472267+jaigansa@users.noreply.github.com>
2020-08-30 17:39:34 +0530

    Update main.js

--- quick_chat/script/main.js
+++ quick_chat/script/main.js
@@ -8,13 +8,13 @@
  \__\_\ \__,_||_| \___||_|\_\ \____||_| |_| \__,_| \__|
 */
 
-// Number Pad
+// button value add to display
 function num(val) {
-    document.getElementById("cellnum").value += val;
+    document.getElementById("display").value += val;
 }
 
 // number only
-function cellnumonly(input) {
+function digitonly(input) {
     var reqex = /[^0-9]/gi;
     input.value = input.value.replace(reqex, "");
 }
@@ -27,15 +27,9 @@ const pre = "https://wa.me/";
 const addtext = "?text=";
 // get datas
 
-
-
-
-
-
-
 function chat_open() {
     var dcod = document.getElementById("dCode").value;
-    var cn = document.getElementById("cellnum").value;
+    var cn = document.getElementById("display").value;
     var txt = document.getElementById("txt").value; // get message data
 
     if (cn.length === 10) {
@@ -50,19 +44,22 @@ function chat_open() {
 
 //clear buttom
 function clears() {
-    x = document.getElementById("cellnum").value;
+    x = document.getElementById("display").value;
     y = x.slice(0, -1);
-    document.getElementById("cellnum").value = y;
+    document.getElementById("display").value = y;
 
 }
 // allclear button
 function allclear() {
-    document.getElementById("cellnum").value = "";
+    document.getElementById("display").value = "";
     document.getElementById("txt").value = "";
 }
 
+
+
+
 //textarea auto height
 function textAreaAdjust(element) {
-    element.style.height = "1px";
-    element.style.height = (1 + element.scrollHeight) + "px";
+    element.style.height = "0.1em";
+    element.style.height = (0.1 + element.scrollHeight) + "px";
 }
