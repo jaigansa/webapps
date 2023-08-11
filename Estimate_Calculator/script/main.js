d5f2b94b4cda9b1b168d00544cf5cb22b9ca7363
jaigansa <>
2023-08-11 11:42:48 +0530

    alter functions

--- Estimate_Calculator/script/main.js
+++ Estimate_Calculator/script/main.js
@@ -1,16 +1,17 @@
-console.log("***Connected main.js File***");
+console.log("init main.js");
 
 /* Import title */
+var title = "Labour Estimate";
 
-var title = document.getElementById("appname");
-var title_text = "Labour Estimate";
-title.innerHTML = "<h1>" + title_text + "</h1>";
-title.style.cssText = " "; //title border
+var newtitle = document.getElementById("appname");
+newtitle.innerHTML = title;
+newtitle.style.cssText = " "; //title css
 
-//get user informations
-
+//get values
 var workhrs = document.getElementById("workHrs");
 var persons = document.getElementById("noofperson");
+
+//manual values
 let profitpercent = 0.25; //profit of work
 
 //get document Elements
@@ -19,92 +20,20 @@ var display = document.getElementById("output");
 
 //get work type
 
-function workType() {
-  var getworktype = document.getElementById("work-type").value;
-  switch (parseInt(getworktype)) {
-    case 1:
+function calculate() {
+
       profit = workhrs.value * persons.value * profitpercent; //profit add 25prercent
       results = workhrs.value * persons.value + profit;
       display.innerHTML = "<h1>Price: " + results + "</h1>";
-      break;
-    case 2:
-      let tools_a = document.getElementById("tools_title");
-      tools_a.innerHTML = "Select Type of Tools";
-      toolsdb();
-      display.innerHTML = "<h1>Labour plus Tools</h1>";
-      break;
-    case 3:
-      display.innerHTML = "<h1>Labour plus Tools plus Material</h1>";
-      break;
 
-    default:
-      display.innerHTML = "<h1>Select Type Of Work.</h1>";
-      break;
   }
+  function reset() {
 
-  //console.log(workhrs.value * persons.value);
+ 
+    display.innerHTML = " ";
+
 }
 
-//select type of tools
-
-function typetools() {
-  let gettypetool = document.getElementById("toolselect").value;
-  let gettoolshelf = document.getElementById("toolshelf");
-  switch (parseInt(gettypetool)) {
-    case 0:
-      
-      let listHT = Object.keys(handtools);
-      console.log(listHT);
-    for (let i=0; i < listHT.length; i++){
-      let htg = listHT[i];
-      console.log(htg); 
-      let hti = document.createElement("input");
-      let htil =document.createElement("label");
-      htil.appendChild(document.createTextNode(htg));
-      hti.appendChild(document.createTextNode(htg));
-      hti.setAttribute("type", "checkbox");
-      gettoolshelf.appendChild(hti);
-      gettoolshelf.appendChild(htil);
-
-    }
-      
-      
-      break;
-    case 1:
-      console.log("powertools");
-      break;
-    case 2:
-      console.log("HeavyTools");
-      console.log(alltools);
-      break;
-
-    default:
-      console.log("alltools");
-      break;
-  }
-}
-
-let heretool = document.getElementById("toolselect");
-
-// create choose tools options
-let newTool = function () {
-  let listhead = Object.keys(alltools);
-  heretool.style.cssText = "display:flex";
-  for (let i = 0; i < listhead.length; i++) {
-    //console.log(listhead[i]);
-    let toolhname = listhead[i];
-    let toolself = document.createElement("option");
-    toolself.appendChild(document.createTextNode(toolhname));
-    toolself.setAttribute("value", i);
-    heretool.appendChild(toolself);
-  }
-  //next shelfs
-  let htool = document.getElementById("tools");
-  let toolselfs = document.createElement("div");
-  toolselfs.setAttribute("id", "toolshelf");
-  htool.insertBefore(toolselfs, htool.lastChild);
-
-};
 
 //tools import from json file
 
