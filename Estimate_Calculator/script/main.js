411d78cfbb4c24b23630b2445dc57679da15859e
jaigansa <>
2023-08-11 12:00:37 +0530

    adding amount field

--- Estimate_Calculator/script/main.js
+++ Estimate_Calculator/script/main.js
@@ -5,62 +5,55 @@ var title = "Labour Estimate";
 
 var newtitle = document.getElementById("appname");
 newtitle.innerHTML = title;
-newtitle.style.cssText = " "; //title css
+newtitle.style.cssText = " ";
+// title css
 
-//get values
+// get values
 var workhrs = document.getElementById("workHrs");
+var workamt = document.getElementById("workamount");
 var persons = document.getElementById("noofperson");
 
-//manual values
-let profitpercent = 0.25; //profit of work
+// manual values
+let profitpercent = 0.25;
+// profit of work
 
-//get document Elements
+// get document Elements
 
 var display = document.getElementById("output");
 
-//get work type
+// get work type
 
 function calculate() {
 
-      profit = workhrs.value * persons.value * profitpercent; //profit add 25prercent
-      results = workhrs.value * persons.value + profit;
-      display.innerHTML = "<h1>Price: " + results + "</h1>";
+    profit = workhrs.value * workamt.value * persons.value * profitpercent; // profit add 25prercent
+    results = workhrs.value * workamt.value * persons.value + profit;
+    display.innerHTML = "<h1>Price: " + results + "</h1>";
 
-  }
-  function reset() {
-
- 
+}
+function reset() {
     display.innerHTML = " ";
-
 }
 
 
-//tools import from json file
+// tools import from json file
 
 let handtools,
-  powertools,
-  heavytools = [],
-  alltools = {};
+    powertools,
+    heavytools = [],
+    alltools = {};
 
 let toolsdb = function () {
-  fetch("res/tools.json")
-    .then((response) => {
-      return response.json();
-    })
-
-    .then((data) => {
-      //console.log(data);
-      handtools = data.handtools;
-      powertools = data.powertools;
-      heavytools = data.heavytools;
-      alltools = data;
-      newTool();
-      typetools();
-    })
-
-    .catch((err) => {
-      // Do something for an error here
+    fetch("res/tools.json").then((response) => {
+        return response.json();
+    }).then((data) => { // console.log(data);
+        handtools = data.handtools;
+        powertools = data.powertools;
+        heavytools = data.heavytools;
+        alltools = data;
+        newTool();
+        typetools();
+    }).catch((err) => { // Do something for an error here
     });
 
-  /* this end fetch */
+    /* this end fetch */
 };
