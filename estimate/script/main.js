e60c65b5ca0f03875f1e53dca3a1b3bbe07601c3
jaigansa <>
2023-08-28 13:16:24 +0530

    estimate update js

--- estimate/script/main.js
+++ estimate/script/main.js
@@ -1,31 +1,20 @@
-console.log("init main.js");
-
 /* Import title */
-var title = "Labour Estimate";
-
 var newtitle = document.getElementById("appname");
+var title = "Labour Estimate";
 newtitle.innerHTML = title;
 newtitle.style.cssText = " ";
-// title css
 
-// get values
+// Variables get
 var workhrs = document.getElementById("workHrs");
 var workamt = document.getElementById("workamount");
 var persons = document.getElementById("noofperson");
+var display = document.getElementById("output");
 
 // manual values
 let profitpercent = 0.25;
-// profit of work
-
-// get document Elements
-
-var display = document.getElementById("output");
-
-// get work type
 
 function calculate() {
-    reset();
-
+    display.innerHTML = "";
     profit = workhrs.value * workamt.value * persons.value * profitpercent; // profit add 25prercent
     results = workhrs.value * workamt.value * persons.value + profit;
     display.innerHTML = results + "/-";
